27bfd601404e7f56613f6f692459b042d0503ef0
Patryk Mikulski <mikulski@patryk.xyz>
2018-05-14 17:32:52 +0200

    cleaning up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyPTE',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/patrk/pyPTE',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Patryk Mikulski',
+    author_email='mikulski@patryk.xyz',
+    description='Pythonic implementation of the Phase Transfer Entropy using NumPy and SciPy'
+)
