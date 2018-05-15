5620ab6211b518f58a8c4eb893b5ce88b95b6d23
Patryk Mikulski <mikulski@patryk.xyz>
2018-05-15 16:59:33 +0200

    docs

--- pyPTE/examples/kuramoto_global.py
+++ pyPTE/examples/kuramoto_global.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def kuramoto(omega_vector, K, N, sigma):
     def f(theta_vector, t):
+        #theta_vector = np.atleast_2d(theta_vector)
         theta_vector = np.atleast_2d(theta_vector)
         d_theta_vector = omega_vector + K/N * np.sum(np.sin(theta_vector - theta_vector.T), 1)
         p = np.random.normal(0, sigma, N)
@@ -11,15 +12,15 @@ def kuramoto(omega_vector, K, N, sigma):
     return f, G
 
 theta0 = np.array([100.0, 50.0, 0.0])
-theta0 = np.arange(0, 1000, 10)
+theta0 = np.arange(0, 10, 1)
 print(theta0)
 omega = np.array([8, 8, 8])
 # omega = np.arange(0, 10, 0.1)
 omega = np.ones_like(theta0)*8
 
-f, G = kuramoto(omega, 10000, 100, 1)
+f, G = kuramoto(omega, 1, 10, 0.1)
 from sdeint import itoint
-tspan = np.linspace(0, 1, 100)
+tspan = np.linspace(0, 1, 1000)
 solution = itoint(f, G, theta0, tspan)
 from matplotlib import pyplot as plt
 plt.plot(tspan, solution)
@@ -34,22 +35,20 @@ solution -= np.pi
 plt.plot(tspan, solution)
 plt.show()
 
-from phase_transfer_entropy import myPTE
-print(solution.shape)
+from pyPTE import myPTE
 # phase = np.swapaxes(solution, 0, 1)
 phase = solution
-print(phase.shape)
-delay = myPTE._get_delay(phase)
+delay = myPTE.get_delay(phase)
 phase2 = phase + np.pi
-binsize = myPTE._get_binsize(phase2)
-bincount = myPTE._get_bincount(binsize)
-dphase = myPTE._get_discretized_phase(phase2, binsize)
+binsize = myPTE.get_binsize(phase2)
+bincount = myPTE.get_bincount(binsize)
+dphase = myPTE.get_discretized_phase(phase2, binsize)
 dPTE, raw_PTE = myPTE.compute_dPTE_rawPTE(dphase, delay)
 
 print(dPTE)
 import seaborn as sns
 cmap = sns.diverging_palette(10, 220, sep=80, n=7)
-sns.heatmap(dPTE, cmap=cmap)
+sns.heatmap(dPTE, cmap=cmap, center=0.5)
 plt.show()
 
 # for yy in y:
