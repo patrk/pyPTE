b2ee93e2f1e8b435b5b98126bb5d0a8564560190
Patryk Mikulski <mikulski@patryk.xyz>
2018-05-29 10:24:29 +0200

    wilcoxon test on PTE

---
+++ pyPTE/utils/stats.py
@@ -0,0 +1,43 @@
+from scipy.stats import wilcoxon
+import numpy as np
+import pandas as pd
+
+def matrix_wilcoxon(x_matrices, y_matrices, alpha = 0.05, bonferroni=True):
+    x = list()
+    y = list()
+    for data_frame in x_matrices:
+        x.append(data_frame.as_matrix())
+    for data_frame in y_matrices:
+        y.append(data_frame.as_matrix())
+
+    p_values = np.zeros_like(x[0])
+    p_mask = np.zeros_like(x[0])
+
+    m, n = p_values.shape
+
+    alpha_c = alpha
+    if bonferroni:
+        alpha_c = alpha*m**2
+
+    for i in range(0, m):
+        temp_x = list()
+        temp_y = list()
+
+        for j in range(0, m):
+            for matrix in x:
+                temp_x.append(matrix[i, j])
+            for matrix in y:
+                temp_y.append(matrix[i, j])
+            s, p = wilcoxon(temp_x, temp_y)
+
+            p_values[i, j] = p
+
+            if p < alpha_c:
+                p_mask[i, j] = False
+            else:
+                p_mask[i, j] = True
+
+    p_values_df = pd.DataFrame(p_values, index=x_matrices[0].index, columns=x_matrices[0].columns)
+    p_mask_df = pd.DataFrame(p_mask, index=x_matrices[0].index, columns=x_matrices[0].columns)
+
+    return p_values_df, p_mask_df
